95c004c570f58c681b60da71c1224d527f05ba93
Ryan Thompson <ryanrthompson10@gmail.com>
2020-08-07 18:50:15 -0500

    Added scalable hover events for images

--- domChange/funcs.js
+++ domChange/funcs.js
@@ -11,3 +11,21 @@ function secondClicked() {
     $("firstPar").className = "open";
     $("secondPar").className = "closed";
 }
+
+function imgHover() {
+    console.log("Hovering on image " + this.alt);
+    this.width = 350;
+}
+
+function imgNotHover() {
+    this.width = 300;
+}
+
+images = document.getElementsByTagName("img");
+console.log(images);
+console.log(images[0])
+images.width = 1000;
+for(let i = 0; i < images.length; i++) {
+    images[i].addEventListener("mouseover", imgHover);
+    images[i].addEventListener("mouseout", imgNotHover);
+}
