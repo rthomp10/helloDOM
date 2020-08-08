6e5de66412a18efcb50e46bf1cc955e638f3b7f8
Ryan Thompson <ryanrthompson10@gmail.com>
2020-08-07 19:12:16 -0500

    Left js style changes to imgs to CSS

--- domChange/funcs.js
+++ domChange/funcs.js
@@ -14,17 +14,15 @@ function secondClicked() {
 
 function imgHover() {
     console.log("Hovering on image " + this.alt);
-    this.width = 350;
 }
 
 function imgNotHover() {
-    this.width = 300;
+    console.log("Not hovering over an image");
 }
 
 images = document.getElementsByTagName("img");
 console.log(images);
 console.log(images[0])
-images.width = 1000;
 for(let i = 0; i < images.length; i++) {
     images[i].addEventListener("mouseover", imgHover);
     images[i].addEventListener("mouseout", imgNotHover);
