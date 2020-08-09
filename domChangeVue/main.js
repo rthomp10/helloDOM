69b22feada1fe38ded2f71144bfacac0d8495f69
Ryan Thompson <ryanrthompson10@gmail.com>
2020-08-09 14:26:04 -0500

    Included iteration through images

--- domChangeVue/main.js
+++ domChangeVue/main.js
@@ -2,9 +2,10 @@ var app = new Vue ({
     el: '#app',
     data: {
         animal: 'Wolves',
-        images: ['./images/wolf.bmp',
-                'https://th.bing.com/th/id/OIP.AqkF_131YtWtRf_u10IMOQHaLH?pid=Api&rs=1',
-                'https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/50417W_DogSidebar_0.jpg?itok=B2nqMim0',
-                'http://wolf.org/wp-content/uploads/2013/08/wcs_10_5_09-66.jpg']
+        images: [
+            { file: './images/wolf.bmp' },
+            { file: 'https://th.bing.com/th/id/OIP.AqkF_131YtWtRf_u10IMOQHaLH?pid=Api&rs=1' },
+            { file: 'https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/50417W_DogSidebar_0.jpg?itok=B2nqMim0' },
+            { file: 'http://wolf.org/wp-content/uploads/2013/08/wcs_10_5_09-66.jpg'}]
     }
 })
