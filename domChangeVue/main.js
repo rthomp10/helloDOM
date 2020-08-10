a5e571ed4d8739ba3d2aa5f5aff8079061c25fb4
Ryan Thompson <ryanrthompson10@gmail.com>
2020-08-10 17:42:10 -0500

    Put the images into an images object

--- domChangeVue/main.js
+++ domChangeVue/main.js
@@ -3,9 +3,13 @@ var app = new Vue ({
     data: {
         animal: 'Wolves',
         images: [
-            { file: './images/wolf.bmp' },
-            { file: 'https://th.bing.com/th/id/OIP.AqkF_131YtWtRf_u10IMOQHaLH?pid=Api&rs=1' },
-            { file: 'https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/50417W_DogSidebar_0.jpg?itok=B2nqMim0' },
-            { file: 'http://wolf.org/wp-content/uploads/2013/08/wcs_10_5_09-66.jpg'}]
+            { src: './images/wolf.bmp',
+              alt: "A cool looking wolf", },
+            { src: 'https://th.bing.com/th/id/OIP.AqkF_131YtWtRf_u10IMOQHaLH?pid=Api&rs=1',
+              alt: "The intimidating wolf", },
+            { src: 'https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/50417W_DogSidebar_0.jpg?itok=B2nqMim0',
+              alt: "Sus wolf" , },
+            { src: 'http://wolf.org/wp-content/uploads/2013/08/wcs_10_5_09-66.jpg',
+              alt: "Cute wolf couple", }]
     }
 })
