15a0a69b058a65debed73a409ac7e1d26a197795
Ryan Thompson <ryanrthompson10@gmail.com>
2020-08-11 17:29:06 -0500

    Click and image to change the title

--- domChangeVue/main.js
+++ domChangeVue/main.js
@@ -4,12 +4,18 @@ var app = new Vue ({
         animal: 'Wolves',
         images: [
             { src: './images/wolf.bmp',
-              alt: "A cool looking wolf", },
+              alt: "A cool looking wolf"},
             { src: 'https://th.bing.com/th/id/OIP.AqkF_131YtWtRf_u10IMOQHaLH?pid=Api&rs=1',
-              alt: "The intimidating wolf", },
+              alt: "The intimidating wolf"},
             { src: 'https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/50417W_DogSidebar_0.jpg?itok=B2nqMim0',
-              alt: "Sus wolf" , },
+              alt: "Sus wolf" },
             { src: 'http://wolf.org/wp-content/uploads/2013/08/wcs_10_5_09-66.jpg',
-              alt: "Cute wolf couple", }]
+              alt: "Cute wolf couple"}],
+        
+    },
+    methods: {
+      titleChange: function (img) {
+        this.animal = img.alt;
+      }
     }
 })
